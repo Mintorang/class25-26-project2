988f788b660f07fc8ba507dc2be7ea5b042c18df
Moyo Ehindero <ehinderomdo1@gmail.com>
2026-02-24 21:43:46 +0000

    Update output.tf

--- tf_files/output.tf
+++ tf_files/output.tf
@@ -1,15 +1,15 @@
 output "app_url" {
-  description = "URL for APP"
+  description = "AppURL"
   value       = "http://${aws_lb.app_lb.dns_name}"
 
 }
 
 output "bastion_ip" {
-  description = "IP for bastion"
+  description = "BsationIP"
   value       = aws_instance.bastion.public_ip
 }
 
 output "appserver_ip" {
-  description = "IP for app"
+  description = "AppIP"
   value       = aws_instance.bastion.private_ip
 }
